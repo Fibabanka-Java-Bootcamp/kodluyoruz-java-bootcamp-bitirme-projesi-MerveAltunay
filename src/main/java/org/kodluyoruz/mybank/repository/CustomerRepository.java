bd0dfd37382290870cccaf6228b0732251a510b8
MerveAltunay <76403911+MerveAltunay@users.noreply.github.com>
2021-02-14 22:30:22 +0300

    onlineBankingSystem

---
+++ src/main/java/org/kodluyoruz/mybank/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package org.kodluyoruz.mybank.repository;
+
+import org.kodluyoruz.mybank.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+
+    Customer findByCitizenshipNumber(long citizenshipNumber);
+
+    Customer findByCustomerId(long customerId);
+
+}
